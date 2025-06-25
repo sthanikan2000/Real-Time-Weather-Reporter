b1d7a9f98fbe2ceb2783758148a896903ea782c2
sthanikan2000 <thanikan.20@cse.mrt.ac.lk>
2025-06-25 10:56:52 +0530

    allowed all request methods

--- backend/main.py
+++ backend/main.py
@@ -28,7 +28,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.allowed_origins,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
